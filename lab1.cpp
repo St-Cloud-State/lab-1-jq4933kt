7a205d3028fae9e1673777b69f422f4376f2da8d
jq4933kt <140684583+jq4933kt@users.noreply.github.com>
2023-07-30 03:11:56 +0000

    Lab1 testing on July 29, 2023

--- lab1.cpp
+++ lab1.cpp
@@ -7,7 +7,7 @@
  const double PI = 3.14159265;
  int main ()
  {
-    double radius, volume;
+    double radius, volume
     cout << "Enter the radius of a sphere: ";
     cin >> radius;volume = 4.0/3.0 * PI * radius * radius * radius;
     cout << "The spheres volume is: " << volume << endl;
