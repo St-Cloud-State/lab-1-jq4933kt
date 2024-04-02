e39fa14e879f54b24ffb13d8d2e5d5a4fe6ea12e
jq4933kt <140684583+jq4933kt@users.noreply.github.com>
2024-04-02 21:40:47 +0000

    April 2, 2024

--- lab1a.cpp
+++ lab1a.cpp
@@ -9,7 +9,7 @@
  {
     double radius, volume
     cout << "Enter the radius of a sphere: ";
-    cin >> radius;volume = 4.0/3.0 * PI * radius * radius * radius 
-    cout << "The spheres volume is: " << volume << endl 
+    cin >> radius;volume = 4.0/3.0 * PI * radius * radius * radius;
+    cout << "The spheres volume is: " << volume << endl;
     return 0;
 }
